69c105523159e26a26a7b930a7cf49044abaf0d8
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-10-26 21:03:09 +0700

    add pagination and build route for bookType

--- controllers/book-type.controller.js
+++ controllers/book-type.controller.js
@@ -1,7 +1,22 @@
 const userModels = require('../models/users.model');
-
+const bookModels = require('../models/books.model');
 
 const bookTypeGet = async function(req, res) {
+  // Get book Data
+  const bookType = req.query.type.split('-').join(' ');
+  const bookData = await bookModels.find(
+      {
+        bookType: bookType,
+      },
+  );
+
+  // Pagination
+  const page = parseInt(req.query.page) || 1;
+  const productPerPage = 16;
+  const begin = (page - 1) * productPerPage;
+  const end = page * productPerPage;
+  const bookArray = bookData.slice(begin, end);
+
   // Check User Cookie
   // If userCookie === true => load User Info
   // Else Load -> raw Index page
@@ -9,10 +24,15 @@ const bookTypeGet = async function(req, res) {
     const userData = await userModels.findOne(
         {_id: req.signedCookies.userId},
     );
-
+    res.locals.currentPage = parseInt(page);
+    res.locals.bookType = req.query.type;
+    res.locals.books = bookArray;
     res.locals.user = userData;
     res.render('book-type');
   } else {
+    res.locals.currentPage = parseInt(page);
+    res.locals.bookType = req.query.type;
+    res.locals.books = bookArray;
     res.render('book-type');
   }
 };
