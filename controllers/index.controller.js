154502ed13f04906dd20555cf3cfe35db79f1c9b
tvqphuoc01 <tvqphuoc.01@gmail.com>
2021-11-13 03:30:15 +0700

    add page visit count and update dashboard

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-unused-vars */
 const userModels = require('../models/users.model');
-
+const fetch = require('node-fetch');
 const indexGet = async function(req, res) {
+  // counting Page view with HIT
+  const response = await fetch('https://api.countapi.xyz/hit/bookStore');
+
   // Check User Cookie
   // If userCookie === true => load User Info
   // Else Load -> raw Index page
--- js/data.js
+++ js/data.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 const ctx = document.getElementById('myChart').getContext('2d');
 const ctx2 = document.getElementById('myChart2').getContext('2d');
 const ctx3 = document.getElementById('myChart3').getContext('2d');
+const ctx4 = document.getElementById('myChart4').getContext('2d');
 
 const userData = usersData; // Load Data from Locals var
 const bookData = booksData; // Load Book Data
@@ -86,9 +88,56 @@ const myChart3 = new Chart(ctx3, {
   },
 });
 
+const myChart4 = new Chart(ctx4, {
+  type: 'bar',
+  data: {
+    labels: ['January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'],
+    datasets: [{
+      label: 'Orders by Month',
+      data: [12, 19, 3, 5, 2, 19, 12, 8, 10, 15, 23, 30],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(56, 235, 76, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(39, 226, 0, 1)',
+      ],
+      borderWidth: 1,
+    }],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 
 myChart2.canvas.parentNode.style.height = '500px';
 myChart2.canvas.parentNode.style.width = '400px';
-myChart3.canvas.parentNode.style.height = '500';
+myChart3.canvas.parentNode.style.height = '500px';
 myChart3.canvas.parentNode.style.width = '400px';
-
+myChart3.canvas.parentNode.style.height = '500px';
